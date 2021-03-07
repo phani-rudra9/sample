c2ae5160e0154bc44027cb1427b13cf29cd0a27e
nadiyavunnam <75115107+nadiyavunnam@users.noreply.github.com>
2021-03-07 20:33:07 +0530

    updated Helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
-// Your First Program
-
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello World!");
+        System.out.println(" - java.version: " + System.getProperty("java.version"));
+        System.out.println(" - java.vm.version: " + System.getProperty("java.vm.version"));
     }
 }
+
